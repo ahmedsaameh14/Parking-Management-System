cab18124e85b4476446c43595184611f865447eb
Ahmed_Sameh <ahmedsaameh14@gmail.com>
2025-08-28 16:50:07 +0300

    Create Navbar + enable Logout button with deleting Token

--- src/app/pages/layout/layout.component.ts
+++ src/app/pages/layout/layout.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [RouterOutlet , RouterLink],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
 export class LayoutComponent {
+constructor(private router:Router){}
 
+  onLogout(){
+    localStorage.removeItem('token')
+    localStorage.removeItem('parkUser')
+
+    this.router.navigate(['/login']);
+  }
 }
